470e8fdc9f129f15ca75a02fc8353227d60afcfb
Mariana Silva <mariana.silva.a46@hotmail.com>
2025-05-13 21:08:50 -0300

    ♻️ Refactor tests for better readability, validation and no use of requests during setup

--- src/test/test-login.ts
+++ src/test/test-login.ts
@@ -1,12 +1,10 @@
-import axios from "axios";
+import { hash } from "bcrypt-ts";
 import { expect } from "chai";
 import jwt from "jsonwebtoken";
 import "mocha";
-import { promisify } from "util";
 import { start, stop } from "../setup";
 import { prisma } from "../setup-db";
-
-const sleep = promisify(setTimeout);
+import axios from "./axios-for-test";
 
 before(async () => {
   await start();
@@ -19,15 +17,23 @@ after(async () => {
 
 describe("POST /auth", function () {
   it(`should return a token valid for ${process.env.TOKEN_TIMEOUT} seconds when login is successful`, async function () {
-    this.timeout(35000);
     const test_data = {
-      id: 1,
       name: "mariana",
       email: "mari@yahoo.com",
       password: "lalala3",
       birthDate: "2004-10-10",
     };
-    await axios.post("http://localhost:3000/users", test_data);
+    await prisma.user.create({
+      data: {
+        name: test_data.name,
+        email: test_data.email,
+        password: await hash(test_data.password, 8),
+        birthDate: new Date(test_data.birthDate),
+      },
+    });
+    const user = await prisma.user.findUnique({
+      where: { email: test_data.email },
+    });
 
     const body = {
       email: test_data.email,
@@ -37,59 +43,49 @@ describe("POST /auth", function () {
     const reply = await axios.post("http://localhost:3000/auth", body);
     const decoded = jwt.verify(reply.data.token, process.env.TOKEN_KEY);
     expect(reply.status).to.be.equal(200);
-    expect(decoded.id).to.be.equal(test_data.id);
-
-    await sleep(1000 * (Number(process.env.TOKEN_TIMEOUT) || 30));
-
-    try {
-      jwt.verify(reply.data.token, process.env.TOKEN_KEY);
-      expect.fail(
-        `Token should be expired after ${process.env.TOKEN_TIMEOUT} seconds`
-      );
-    } catch (error) {
-      expect(error.name).to.be.equal("TokenExpiredError");
-    }
+    expect(decoded.id).to.be.equal(user.id);
+    expect(decoded.exp - decoded.iat).to.be.equal(
+      Number(process.env.TOKEN_TIMEOUT) ?? 30
+    );
   });
   it("should return an error if the email is not registered", async function () {
     const body = {
       email: "mari@gmail.com",
       password: "senha123",
     };
-    try {
-      await axios.post("http://localhost:3000/auth", body);
-    } catch (error) {
-      expect(error.response.status).to.be.equal(400);
-      expect(error.response.data).to.be.deep.equal({
-        message: "Email not registered on platform",
-        code: "EML_02",
-      });
-    }
+    const response = await axios.post("http://localhost:3000/auth", body);
+    expect(response.status).to.be.equal(400);
+    expect(response.data).to.be.deep.equal({
+      message: "Email not registered on platform",
+      code: "EML_02",
+    });
   });
   it("should return an error if the password is incorrect", async function () {
     const test_data = {
-      id: 10,
       name: "beatriz",
       email: "bia@yahoo.com",
       password: "lalala3",
       birthDate: "2005-02-27",
     };
-
-    await axios.post("http://localhost:3000/users", test_data);
+    await prisma.user.create({
+      data: {
+        name: test_data.name,
+        email: test_data.email,
+        password: await hash(test_data.password, 8),
+        birthDate: new Date(test_data.birthDate),
+      },
+    });
 
     const body = {
       email: test_data.email,
       password: "wrongpassword",
     };
 
-    try {
-      await axios.post("http://localhost:3000/auth", body);
-    } catch (error) {
-      expect(error.response.status).to.be.equal(400);
-      expect(error.response.data).to.be.deep.equal({
-        message: "Wrong password. Try again",
-        code: "PSW_03",
-      });
-    }
+    const response = await axios.post("http://localhost:3000/auth", body);
+    expect(response.status).to.be.equal(400);
+    expect(response.data).to.be.deep.equal({
+      message: "Wrong password. Try again",
+      code: "PSW_03",
+    });
   });
-  it("the token returned should be expired in 30 seconds", async function () {});
 });
