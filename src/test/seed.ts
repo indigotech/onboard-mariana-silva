8343d016e4c0098490c6755aede7a3a22f8cc540
Mariana Silva <mariana.silva.a46@hotmail.com>
2025-05-14 09:24:34 -0300

    🌱Add seed script

---
+++ src/test/seed.ts
@@ -0,0 +1,39 @@
+import { faker } from "@faker-js/faker";
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcrypt-ts";
+
+const prisma = new PrismaClient();
+
+faker.seed(0);
+
+async function main() {
+  await prisma.$connect();
+  console.log("Start seeding...");
+
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY`;
+
+  const users = await Promise.all(
+    Array.from({ length: 50 }).map(async () => ({
+      name: faker.person.firstName(),
+      email: faker.internet.email(),
+      password: await hash(faker.internet.password(), 8),
+      birthDate: faker.date.birthdate().toISOString(),
+    }))
+  );
+
+  const createdUsers = await prisma.user.createMany({
+    data: users,
+  });
+
+  console.log(`Inserted ${createdUsers.count} users`);
+  console.log("Seeding completed.");
+}
+
+main()
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
