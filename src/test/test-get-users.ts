80d5b37cadf2463518f296699069680a9ba84ddc
Mariana Silva <mariana.silva.a46@hotmail.com>
2025-05-15 19:06:48 -0300

    Remove before and after from individual test file

--- src/test/test-get-users.ts
+++ src/test/test-get-users.ts
@@ -4,19 +4,9 @@ import { prisma } from "../setup-db";
 
 import { expect } from "chai";
 import { getNextUser, getPreviousUser } from "../server/route-get-users";
-import { start, stop } from "../setup";
 import utils, { configRequestToken } from "./test-utils";
 
 const { testAxios: axios, validToken } = utils;
-
-before(async () => {
-  await start();
-});
-
-after(async () => {
-  await prisma.user.deleteMany();
-  await stop();
-});
 
 function config(token: string) {
   return {
