661313e33c7667e6bbe1512acd341043f303381b
Mariana Silva <mariana.silva.a46@hotmail.com>
2025-05-18 12:43:41 -0300

    🌱 Update seed script

--- src/test/seed.ts
+++ src/test/seed.ts
@@ -6,33 +6,50 @@ const prisma = new PrismaClient();
 
 faker.seed(0);
 
-async function main() {
+async function executeSeeding() {
   console.log("Start seeding...");
 
-  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY`;
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
 
   const users = await Promise.all(
-    Array.from({ length: 50 }).map(async (_, idx, __) => ({
-      name: faker.person.firstName(),
-      email: `${idx}--${faker.internet.email()}`,
-      password: await hash(faker.internet.password(), 8),
-      birthDate: faker.date.birthdate().toISOString(),
-    }))
+    Array.from({ length: 50 }).map(async (_, idx) => {
+      return prisma.user.create({
+        data: {
+          name: faker.person.firstName(),
+          email: `${idx}--${faker.internet.email()}`,
+          password: await hash(faker.internet.password(), 8),
+          birthDate: faker.date.birthdate().toISOString(),
+          addresses: {
+            create: Array.from({
+              length: faker.number.int({ min: 0, max: 4 }),
+            }).map(() => ({
+              CEP: faker.location.zipCode("xxxxx-xxx"),
+              street: faker.location.street(),
+              number: faker.number.int({ min: 1, max: 8000 }),
+              complement: faker.location.secondaryAddress(),
+              neighborhood: faker.location.county(),
+              city: faker.location.city(),
+              state: faker.location.state(),
+            })),
+          },
+        },
+      });
+    })
   );
 
-  const createdUsers = await prisma.user.createMany({
-    data: users,
-  });
-
-  console.log(`Inserted ${createdUsers.count} users`);
+  console.log(`Inserted ${users.length} users`);
   console.log("Seeding completed.");
 }
 
-main()
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+async function seed() {
+  executeSeeding()
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+}
+
+await seed();
